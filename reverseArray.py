d62ce0797db7f9c88925b7f4e7748847623c42d2
Patrick Adamson <nppat@hotmail.com>
2016-06-15 19:24:02 -0500

    add reverseArray

--- reverseArray.py
+++ reverseArray.py
@@ -1,5 +1,14 @@
 '''
 Reverse Array
 
-Given a numerical array, reverse the order of the values. The reversed array should have the same length, with existing elements moved to other indices so that the order of elements is reversed. 
+Given a numerical array, reverse the order of the values.
+The reversed array should have the same length, with existing
+elements moved to other indices so that the order of elements is reversed. 
 '''
+myList = [1,2,3,4,5,6,7,8,9,10] # Create a list
+
+def reverseList(a):	# define a function with one argument
+	new_list = a[::-1] #create a new variable that takes the argument that is passed and reverses it using slicing
+	print new_list # print the new_list variable
+
+reverseList(myList) # call the function with the argument myList
