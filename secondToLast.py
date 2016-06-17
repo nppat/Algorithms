b040b38083060af549623b667607a2693023ee38
Patrick Adamson <nppat@hotmail.com>
2016-06-17 17:17:48 -0500

    initial commit

---
+++ secondToLast.py
@@ -0,0 +1,14 @@
+'''
+Second-to-Last
+
+Given an array, return the second-to-last element.  
+
+'''
+
+# myList = [1,2,3,4,5,6,7,8,9,10]
+myList = [1,2,3]
+
+def returnSecondToLast(a): # define function with one parameter
+	print a[len(a) - 2] # print out the 2nd to last element in the list
+
+returnSecondToLast(myList) # call function, pass in myList
